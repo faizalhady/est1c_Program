3bfbe5b6f75f8be78e757a2ffe6f4e486423535e
faizalhady <syedfaizalhady@gmail.com>
2025-08-28 14:24:29 +0800

    28/8

--- obj/Debug/net8.0/eST1C_ProgramImporter.AssemblyInfo.cs
+++ obj/Debug/net8.0/eST1C_ProgramImporter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eST1C_ProgramImporter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0775006fac97d8f3ee6dc7040b051eda4909d423")]
 [assembly: System.Reflection.AssemblyProductAttribute("eST1C_ProgramImporter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eST1C_ProgramImporter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
